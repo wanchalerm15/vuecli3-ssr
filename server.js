7501ec21edc2c7bc0783b5925b4b31053f4e1f45
Wanchaloem Laoket <ttvone@hotmail.com>
2018-08-16 17:12:19 +0700

    fix server .js

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const template = fs.readFileSync(path.join(__dirname, './dist/index.html'), 'utf8');
 const serverBundle = require('./dist/vue-ssr-server-bundle.json');
 const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
-    template: `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head><title>Hello</title></head>
-      <body>
-        <!--vue-ssr-outlet-->
-      </body>
-    </html>`
+    template: template.replace('<div id=app></div>', '<!--vue-ssr-outlet-->')
 });
 
-const context = {
-    url: '/',
-    state: {}
-};
-
-renderer.renderToString(context, (err, html) => {
-    console.log(html);
+const express = require('express');
+const server = express();
+server.use(express.static(path.join(__dirname, './dist')));
+server.get('**', (req, res) => {
+    const context = {
+        url: req.url
+    };
+    renderer.renderToStream(context).pipe(res);
 });
+server.listen(3000, () => console.log('Server started.'))
